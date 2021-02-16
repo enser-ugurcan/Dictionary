626fe143f012ea46f865ddba92254257549b532c
Enser Uğurcan Ateşçakmak <enser3678@outlook.com>
2021-02-16 18:41:50 +0300

    Add project files

---
+++ Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<T,K>
+    {
+        T[] Names;
+        K[] Numbers;
+
+        T[] TempNames;
+        K[] TempNumbers;
+        public Dictionary()
+        {
+            Names = new T[0];
+            Numbers = new K[0];
+        }
+        public void Add(T names , K numbers)
+        {
+            TempNames = Names;
+            TempNumbers = Numbers;
+
+            Names = new T[Names.Length + 1];
+            Numbers = new K[Numbers.Length + 1];
+
+            for (int i = 0; i < TempNames.Length; i++)
+            {
+                TempNames[i] = Names[i];
+            }
+            for (int i = 0; i < TempNumbers.Length; i++)
+            {
+                TempNumbers[i] = Numbers[i];
+            }
+            Names[Names.Length - 1] = names;
+            Numbers[Numbers.Length - 1] = numbers;
+
+        }
+        public int Count
+        {
+            get { return Numbers.Length; }
+        }
+        public int countt
+        {
+            get { return Names.Length; }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string,int>();
+            dictionary.Add("İstanbul", 1);
+            dictionary.Add("Ankara", 2);
+            dictionary.Add("İzmir", 3);
+            Console.WriteLine(dictionary.Count);
+            Console.WriteLine(dictionary.countt);
+        }
+    }
+}
